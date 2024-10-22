144edc18c943e81644b8170ecd7698138f01a99f
Akshay L <110522215+akshay-kumarl@users.noreply.github.com>
2024-10-22 19:41:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,6 @@ terraform {
     key            = "terraform.tfstate" 
     region         = "ap-south-1" 
     encrypt        = true
-    dynamodb_table = "terraform-lock-table" 
+   # dynamodb_table = "terraform-lock-table" 
   }
 }
