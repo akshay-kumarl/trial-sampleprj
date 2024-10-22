13afd0ed9b8dc43530d9426cdb1e50e5171f6f51
Akshay L <110522215+akshay-kumarl@users.noreply.github.com>
2024-10-22 18:29:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,25 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = ["sg-05cd9bab43a70250e"]
   key_name = "keypairbylearnjan"
 }
+
+#output
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+#backend
+terraform {
+  backend "s3" {
+    bucket         = "samplebuckt00" 
+    key            = "samplebuckt00/terraform.tfstate" #terraform.tfstate is compulsory file 
+    region         = "ap-south-1" 
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table" 
+  }
+}
