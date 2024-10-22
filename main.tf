964da71a0e026a0623c8f567b3fddf92b9856da3
Akshay L <110522215+akshay-kumarl@users.noreply.github.com>
2024-10-22 15:21:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"  # Specify the Mumbai region
+  access_key = var.access
+  secret_key = var.access
+}
+
+# Create an EC2 instance
+resource "aws_instance" "web" {
+  ami           = "ami-08718895af4dfa033"  # Use the latest Ubuntu AMI ID
+  instance_type = "t2.micro"              # Instance type
+  tags = {
+    Name = "Mumbai-linux-EC2-Instance"  # Tag the instance
+  }
+  vpc_security_group_ids = ["sg-05cd9bab43a70250e"]
+  key_name = "keypairbylearnjan"
+}
